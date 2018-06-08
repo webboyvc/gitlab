20dcbc092f4716cef4e5960ec622c1fe0eb61a54
xHang <dreampet@gmail.com>
2018-06-08 08:53:01 +0800

    补全项目设置翻译

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -32,7 +32,7 @@ module ServicesHelper
   def service_save_button(service)
     button_tag(class: 'btn btn-save', type: 'submit', disabled: service.deprecated?) do
       icon('spinner spin', class: 'hidden js-btn-spinner') +
-        content_tag(:span, 'Save changes', class: 'js-btn-label')
+        content_tag(:span, '保存修改', class: 'js-btn-label')
     end
   end
 
